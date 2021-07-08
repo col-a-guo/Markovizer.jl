3a31686e27fc972125574515a8030b32982c8df0
Collin A Guo <bunjuku8@gmail.com>
2021-07-08 13:01:04 -0700

    Co-authored-by: Alok Singh <alok@users.noreply.github.com>

---
+++ markovizer_test.jl
@@ -0,0 +1,85 @@
+
+using Symbolics
+#=
+macro wrap_var(var_args...)
+    v = Symbol(var_args...)
+    @variables a 2
+    #println(b2)
+end
+=#
+function main()
+
+    param_array = []
+    x = [[1, 2], [2, 3], [3,4], [5,7], [6,2], [3, 7]]
+    
+    model = []
+
+    labels = [1.5, 2.5, 3.5, 6, 4, 5]
+
+    layers = [2, 1]
+    
+    initialθ = Num[0.5 0.5]
+
+    data_size = 2
+    data_num = 2
+
+    weights_size = 2
+    lr = 0.1
+
+
+    @variables a[2:1]
+
+
+    θ = initialθ
+    θ = [a[1] a[2]]
+    for epoch in 1:100
+        for (i, datum) in enumerate(x)
+            prediction = [θ[1]*datum[1] + θ[2]*datum[2]]
+            adjust = Symbolics.jacobian([(a[1]*datum[1] + a[2]*datum[2] - labels[i])^2], [a[1], a[2]])
+            println("grads")
+            println(adjust)
+            println("weights")
+            println(θ)
+            θ += lr*adjust
+        end
+    end
+    substitute.(θ, (Dict(a[1] => 0.8, a[2] => 0.8),))
+
+    #TODO: make this generate variable variables e.g. x_3_1 through x_3_5 to represent weights of neuron 3 connected to a layer of 5
+    #future TODO: 
+    #=
+    for i in 1:size(layers)[1]-1
+        for neuron_1 in 1:layers[i]
+            for neuron_2 in 1:layers[i+1]
+                @variables wrap_var(alphabet_list[i])[layers[i]:layers[i+1]]
+            end
+        end
+    end
+
+    for i in 1:size(layers)[1]-1
+        for neuron_1 in 1:layers[i]
+            for neuron_2 in 1:layers[i+1]
+                @variables wrap_var(alphabet_list[i])[layers[i]:layers[i+1]]
+            end
+        end
+    end
+    #assume variables are listed as x_n_m, in list_of_vars
+
+    #TODO: make better by deriving in the loop?
+
+    for i in 1:size(layers)[1]-1
+        list_of_weight_lists = []
+        for neuron_1 in 1:layers[i]
+            weight_list = []
+            for neuron_2 in 1:layers[i+1]
+                push!(weight_list, alphabet_list[i][neuron_1, neuron_2])
+            end
+            push!(list_of_weight_lists, weight_list)
+        end
+    end
+
+=#
+
+
+end
+main()
